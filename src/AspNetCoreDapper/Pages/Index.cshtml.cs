0981efd6d73ccdc98a8bdb7e1729d55597c32d52
Eric Anderson <elanderson@gmail.com>
2019-01-02 06:53:41 -0600

    Add Dapper NuGet  and DB connection string

--- src/AspNetCoreDapper/Pages/Index.cshtml.cs
+++ src/AspNetCoreDapper/Pages/Index.cshtml.cs
@@ -4,11 +4,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace AspNetCoreDapper.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly IConfiguration _configuration;
+
+        public IndexModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public void OnGet()
         {
 
