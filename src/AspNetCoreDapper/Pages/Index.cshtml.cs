589ba0412e71e01971c6df4a68cd608b9717defd
Eric Anderson <elanderson@gmail.com>
2019-01-03 06:18:53 -0600

    Finish example

--- src/AspNetCoreDapper/Pages/Index.cshtml.cs
+++ src/AspNetCoreDapper/Pages/Index.cshtml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using AspNetCoreDapper.Models;
+using Dapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
@@ -19,7 +22,32 @@ namespace AspNetCoreDapper.Pages
 
         public void OnGet()
         {
+            using (var connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+            {
+                connection.Open();
 
+                if (connection.QueryFirstOrDefault<int?>(@"SELECT Id 
+                                                               FROM Contacts 
+                                                               WHERE Name = @Name",
+                                                         new {Name = "Charlie Plumber"}) == null)
+                {
+                    connection.Execute(@"INSERT INTO Contacts (Name, Address, City, Subregion, Email) 
+                                             VALUES (@Name, @Address, @City, @Subregion, @Email)",
+                                       new Contact
+                                       {
+                                           Name = "Charlie Plumber",
+                                           Address = "123 Main St",
+                                           City = "Nashville",
+                                           Subregion = "TN",
+                                           Email = "cplumber@fake.com"
+                                       });
+                }
+
+                var charile = connection.QueryFirstOrDefault<Contact>(@"SELECT Id, Name, Address, City, Subregion, Email 
+                                                                            FROM Contacts 
+                                                                            WHERE Name = @Name",
+                                                                      new {Name = "Charlie Plumber"});
+            }
         }
     }
 }
